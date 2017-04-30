d03c01144d4cfa837681e356c24a735ecd20f9b8
NilsKaper <nk075@hdm-stuttgart.de>
2017-04-30 12:31:55 +0200

    Create Essential Methods in MarketplaceAdministration
    created Methods for administration of Business-Objects in package
    de.hdm.marketPlace.shared

--- src/de/hdm/marketPlace/shared/MarketplaceAdministration.java
+++ src/de/hdm/marketPlace/shared/MarketplaceAdministration.java
@@ -1,5 +1,74 @@
 package de.hdm.marketPlace.shared;
 
+import java.util.*;
+import de.hdm.marketPlace.shared.bo.*;
+
 public class MarketplaceAdministration {
+	
+	//Methoden zur Verwaltung des Marketplace
+	public void createMarketplace(){
+		
+	}
+	public void updateMarketplace(){
+		
+	}
+	public void deleteMarketplace(){
+		
+	}
+	
+	//Methoden zur Verwaltung der Attribute
+	public void createAttribute(){
+		
+	}
+	public void updateAttribute(){
+		
+	}
+	public void deleteAttribute(){
+		
+	}
+	
+	//Methoden zur Verwaltung von Benutzerprofilen
+	public void createUserProfile(){
+		
+	}
+	public void updateUserProfile(){
+		
+	}
+	public void deleteUserProfile(){
+		
+	}
+	
+	//Methoden zur Verwaltung von Ausschreibungen
+	public void createTender(){
+		
+	}
+	public void updateTender(){
+		
+	}
+	public void deleteTender(){
+		
+	}
+	public Vector <Tender> showAllTender(){
+		Vector <Tender> allTenders = new Vector<Tender>();
+		return allTenders;
+	}
+	
+	//Methoden zur Verwaltung von Bewerbungen
+	public void createApplication(){
+		
+	}
+	public void updateApplication(){
+		
+	}
+	public void deleteApplication(){
+		
+	}
+	//Vektor zum Ausgeben aller Bewerbungen
+	public Vector <Application> showAllApplications(){
+		Vector <Application> allApplications= new Vector<Application>();
+		
+		return allApplications;
+	}
+	
 
 }
