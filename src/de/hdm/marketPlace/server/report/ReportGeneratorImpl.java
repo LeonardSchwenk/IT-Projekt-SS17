ca83999722a2956227c5bdd6d94d887e03549602
MiroGoettler <mg154@hdm-stuttgart.de>
2017-05-01 13:49:24 +0200

    ReportGenerator Classes
    Created all missing classes for the ReportGenerator, part wrote the code
    Interface classes in ReportGenerator.java where written

---
+++ src/de/hdm/marketPlace/server/report/ReportGeneratorImpl.java
@@ -0,0 +1,11 @@
+package de.hdm.marketPlace.server.report;
+
+import java.util.Date;
+
+public class ReportGeneratorImpl extends RemotServiceServlet implements ReportGenerator{ //Wo findet sich RemotService Servlet??
+
+	// Erzeugt später die fertigen Reports und gibt sich mit Hilfe der tabellen aus
+	
+	
+	
+}
