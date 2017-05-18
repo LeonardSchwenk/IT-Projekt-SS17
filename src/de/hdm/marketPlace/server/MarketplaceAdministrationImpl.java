e445e64562263dc874452827b66adb25be8d4ecf
NilsKaper <nk075@hdm-stuttgart.de>
2017-05-18 11:29:04 +0200

    import relevant packages for MarketplaceAdminstrationImpl
    import relevant packages of BusinessObjects and DataBaseConnection,
    implemented interface MarketplaceAdminstration

--- src/de/hdm/marketPlace/server/MarketplaceAdministrationImpl.java
+++ src/de/hdm/marketPlace/server/MarketplaceAdministrationImpl.java
@@ -1,5 +1,16 @@
 package de.hdm.marketPlace.server;
 
-public class MarketplaceAdministrationImpl {
+import de.hdm.marketPlace.shared.*;
+import de.hdm.marketPlace.shared.bo.*;
+import de.hdm.marketPlace.server.db.*;
 
+import java.util.Vector;
+import java.util.ArrayList;
+
+
+
+public class MarketplaceAdministrationImpl extends RemoteServiceServlet implements MarketplaceAdministration {
+
+	
+	
 }
