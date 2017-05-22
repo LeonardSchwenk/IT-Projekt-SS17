21c962214682c92de0ef8ba9b0f437db1decbb23
NilsKaper <nk075@hdm-stuttgart.de>
2017-05-22 10:36:25 +0200

    created Mappers in class MarketplaceAdministration

--- src/de/hdm/marketPlace/server/MarketplaceAdministrationImpl.java
+++ src/de/hdm/marketPlace/server/MarketplaceAdministrationImpl.java
@@ -11,6 +11,39 @@ import java.util.ArrayList;
 
 public class MarketplaceAdministrationImpl extends RemoteServiceServlet implements MarketplaceAdministration {
 
+	//Referenz auf den zugehörigen ProjektMarktplatz
+	private ProjectMarketplace marketplace = null;
+	
+	//Anlegen der Mapper
+	private ApplicationMapper apMapper = null;
+	
+	private AttributeMapper atMapper = null;
+	
+	private ParticipationMapper paMapper = null;
+	
+	private ProjectMapper prMapper = null;
+	
+	private ProjectMarketplaceMapper pmMapper = null;
+	
+	private RatingMapper raMapper = null;
+	
+	private TenderMapper teMapper = null;
+	
+	private UserMapper usMapper = null;
+	
+	private UserProfileMapper upMapper = null;
+	
+	
+	
+	//Anlegen eines Null-Argument-Constructors für die Klasse MarketplaceAdministrationImpl
+	public MarketplaceAdministrationImpl() throws IllegalArgumentException {
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 }
