b0832e3036bb8fe86715bb9f917eb6f1dc292b2a
ArthurZebrala <Zebrala@Zebrala-NB.Speedport_W_724V_Typ_A_05011603_00_007>
2017-05-23 19:09:31 +0200

    Impl

--- src/de/hdm/marketPlace/server/LoginServiceImpl.java
+++ src/de/hdm/marketPlace/server/LoginServiceImpl.java
@@ -1,5 +1,32 @@
 package de.hdm.marketPlace.server;
 
-public class LoginServiceImpl {
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import de.hdm.marketPlace.shared.bo.*;
+
+
+public class LoginServiceImpl extends RemoteServiceServlet implements
+    LoginService {
+
+// Ausführung 
+
+  public User login(String requestUri) {
+    UserService userService = UserServiceFactory.getUserService();
+    User user = userService.getCurrentUser();
+    User loginInfo = new User();
+
+    if (user != null) {
+      loginInfo.setLoggedIn(true);
+      loginInfo.setGoogleId(user.getEmail());
+      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+    } else {
+      loginInfo.setLoggedIn(false);
+      loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+    }
+    return loginInfo;
+  }
 
 }
