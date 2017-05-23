7b8b60f80ab1bf06dac9bb589ef56e404d3f64ef
FabianHupfeld <fh085@hdm-stuttgart.de>
2017-05-23 14:34:38 +0200

    Edited User class
    Changed name to first and lastname

--- src/de/hdm/marketPlace/shared/bo/User.java
+++ src/de/hdm/marketPlace/shared/bo/User.java
@@ -7,29 +7,44 @@ public class User extends BusinessObject {
 	
 	//Attribute der Klasse 
 	
-	private String name = "";
+	private String firstname = "";
+	private String lastname = "";
 	private int projectmarketplaceRef = 0; 
 	
 	/*Alle Get und Set Methoden zum auslesen und setzen der 
 	Attribute */
 	
-	public void setName (String name){
-		this.name = name;
-	}
-	public String getName (){
-		return name;
-	}
+	
+	
 	public int getProjectmarketplaceRef() {
 		return projectmarketplaceRef;
 	}
+	
 	public void setProjectmarketplaceRef(int projectmarketplaceRef) {
 		this.projectmarketplaceRef = projectmarketplaceRef;
 	}
-		
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
 	//Textuelle Ausgabe der jeweiligen Nutzerprofil-Instanz  
-	
+
+
 		public String toString() {
-			    return super.toString() + "Name:  " + this.name;
+			    return super.toString() + "FirstName:  " + this.firstname +
+			    		"LastName: " + this.lastname;
 			  }
 	}
 
