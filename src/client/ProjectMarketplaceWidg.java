db04ab135a4b06d25c23a371470edc1db7a8904f
ArthurZebrala <arthurzebrala@web.de>
2017-05-29 23:55:27 +0200

    Anfang PM Widgets

---
+++ src/client/ProjectMarketplaceWidg.java
@@ -0,0 +1,25 @@
+package client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+
+
+
+public class ProjectMarketplaceWidget extends Composite {
+	
+	Button newProjektmarktplatz = new Button("Projektmarktplatz anlegen");
+	Button deleteProjektmarktplatz = new Button("Projektmarktplatz löschen");
+	Button seeProjektmarktplatz = new Button("Projektmarktplatz ansehen"); 
+	Button updateProjektmarktplatz = new Button("Projektmarktplatz bearbeiten");
+	
+	
+}
