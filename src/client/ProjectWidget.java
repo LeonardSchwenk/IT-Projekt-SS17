b5716c3d451f6a3e260076130e6270da7aaa8d62
ArthurZebrala <arthurzebrala@web.de>
2017-05-29 23:51:55 +0200

    Anfang ProjectButtons

---
+++ src/client/ProjectWidget.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.util.Date;
+import java.util.Vector;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+
+
+
+public class ProjectWidget extends Composite{
+	
+	
+	Button newProject = new Button("Projekt anlegen");
+	Button deleteProjektmarktplatz = new Button("Projekt löschen");
+	Button seeProjektmarktplatz = new Button("Projektmarktplatz ansehen"); 
+	Button updateProjektmarktplatz = new Button("Projektmarktplatz bearbeiten");
+	//Clickhandler
+
+
+
+	}
