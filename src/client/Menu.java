ae1ab8e124e3cdd985177fa61a7635a2630f9d9c
ArthurZebrala <arthurzebrala@web.de>
2017-05-29 23:43:28 +0200

    Menu

--- src/client/Menu.java
+++ src/client/Menu.java
@@ -1,7 +1,54 @@
 package client;
 
-//imports + packages
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class Menu {
 
+public class Menu extends Composite{
+	
+	//Auffangwidget		
+	VerticalPanel menuPanel = new VerticalPanel();
+	//Verbindung zur Impl
+	//ProjectMarketplaceAdministrationAsync verwaltung = ClientsideSettings.getProjectMarketplaceAdministration();
+
+	Button homeButton = new Button("Startseite");
+	Button profilButton = new Button("User");
+	Button pMarketplace = new Button("Projektmarktplatz");
+	Button projekte = new Button("Projekte");
+	Button ausschreibungen = new Button("Ausschreibungen");
+	Button bewerbungen = new Button("Bewerbungen");
+	Button report = new Button("Report");
+	Button impressumButton = new Button("Impressum");
+	Button logout = new Button("Logout");
+	
+
+	public Menu() {
+
+		menuPanel.add(homeButton);
+		menuPanel.add(profilButton);
+		menuPanel.add(pMarketplace);
+		menuPanel.add(projekte);
+		menuPanel.add(bewerbungen);
+		menuPanel.add(ausschreibungen);
+		menuPanel.add(report);
+		menuPanel.add(impressum);
+ 		menuPanel.add(logout);
+
+		
+		
+
+		// Layout Button einfügen--> setPixelSize
+// Abstand zwischen Buttons
+// verknüpfung mit css Datei
+
+		
+		//sämtliche Clickhandler
+	}
+void init(menuPanel);
 }
