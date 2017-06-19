3dfd3a8b4e2245cece11272873e882fe43900299
LeonardSchwenk <schwenkleonard@gmail.com>
2017-06-19 12:49:13 +0200

    no merge confict

--- src/de/hdm/marketPlace/client/Projects.java
+++ src/de/hdm/marketPlace/client/Projects.java
@@ -14,47 +14,8 @@ public class Projects extends Composite  {
 	
 	
 	
-	VerticalPanel vP= new VerticalPanel();
-	HorizontalPanel hP = new HorizontalPanel();
-	
-	MainView main;
-	
-	Button showCreateProjectButton = new Button("Create Project");
-	Button showUpdateProjectButton = new Button("Update Project");
-	Button showDeleteProjectButton = new Button("Delete Project");
-	
-	public Projects (MainView menu){
-		
-		initWidget(this.vP);
-		this.main = main;
 		
 		
-		
-		
-		
-
-	/*	hP.add(showCreateProjectButton);
-		showCreateProjectButton.setWidth("200px");
-		showCreateProjectButton.setStylePrimaryName("menu-btn");
-		
-		hP.add(showUpdateProjectButton);
-		showUpdateProjectButton.setWidth("200px");
-		showUpdateProjectButton.setStylePrimaryName("menu-btn");
-		
-		hP.add(showDeleteProjectButton);
-		showDeleteProjectButton.setWidth("200px");
-		showDeleteProjectButton.setStylePrimaryName("menu-btn");
-		
-		*/
-		
-		
-		
-		
-		
-		
-		
-}
-	
 
 	
 }
